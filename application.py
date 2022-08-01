a8c6f20212ec456535ef66f9bc6f14d0e86acaa8
hothaifa zoubi <45634350+hothaifa96@users.noreply.github.com>
2022-08-01 20:21:36 +0300

    Add files via upload

--- application.py
+++ application.py
@@ -1,8 +1,137 @@
 from flask import Flask
+import copy
 from flask import request, redirect, url_for
 import json
 
 app = Flask(__name__)
+
+dic_currencies = {1: [
+    {"ANG": 1.7900},
+    {"USD": 1},
+    {"AED": 3.6725},
+    {"AFN": 89.6941},
+    {"ALL": 114.2457},
+    {"AOA": 429.9365},
+    {"ARS": 129.051},
+    {"AMD": 413.2282},
+    {"AUD": 1.4426},
+    {"AWG": 1.7900},
+    {"AZN": 1.6941},
+    {"BAM": 1.9178},
+    {"BBD": 2.0000},
+    {"BTN": 79.9633},
+    {"EUR": 0.9806},
+    {"FJD": 2.2014},
+    {"GBP": 0.8353},
+    {"ILS": 3.4311}], 2: [
+    {"ANG": 1.7900},
+    {"USD": 1},
+    {"AED": 3.6725},
+    {"AFN": 89.6941},
+    {"ALL": 114.2457},
+    {"AOA": 429.9365},
+    {"ARS": 129.051},
+    {"AMD": 413.2282},
+    {"AUD": 1.4426},
+    {"AWG": 1.7900},
+    {"AZN": 1.6941},
+    {"BAM": 1.9178},
+    {"BBD": 2.0000},
+    {"BTN": 79.9633},
+    {"EUR": 0.9806},
+    {"FJD": 2.2014},
+    {"GBP": 0.8353},
+    {"ILS": 3.4311}], 3: [
+    {"ANG": 1.7900},
+    {"USD": 1},
+    {"AED": 3.6725},
+    {"AFN": 89.6941},
+    {"ALL": 114.2457},
+    {"AOA": 429.9365},
+    {"ARS": 129.051},
+    {"AMD": 413.2282},
+    {"AUD": 1.4426},
+    {"AWG": 1.7900},
+    {"AZN": 1.6941},
+    {"BAM": 1.9178},
+    {"BBD": 2.0000},
+    {"BTN": 79.9633},
+    {"EUR": 0.9806},
+    {"FJD": 2.2014},
+    {"GBP": 0.8353},
+    {"ILS": 3.4311}], 4: [
+    {"ANG": 1.7900},
+    {"USD": 1},
+    {"AED": 3.6725},
+    {"AFN": 89.6941},
+    {"ALL": 114.2457},
+    {"AOA": 429.9365},
+    {"ARS": 129.051},
+    {"AMD": 413.2282},
+    {"AUD": 1.4426},
+    {"AWG": 1.7900},
+    {"AZN": 1.6941},
+    {"BAM": 1.9178},
+    {"BBD": 2.0000},
+    {"BTN": 79.9633},
+    {"EUR": 0.9806},
+    {"FJD": 2.2014},
+    {"GBP": 0.8353},
+    {"ILS": 3.4311}], 5: [
+    {"ANG": 1.7900},
+    {"USD": 1},
+    {"AED": 3.6725},
+    {"AFN": 89.6941},
+    {"ALL": 114.2457},
+    {"AOA": 429.9365},
+    {"ARS": 129.051},
+    {"AMD": 413.2282},
+    {"AUD": 1.4426},
+    {"AWG": 1.7900},
+    {"AZN": 1.6941},
+    {"BAM": 1.9178},
+    {"BBD": 2.0000},
+    {"BTN": 79.9633},
+    {"EUR": 0.9806},
+    {"FJD": 2.2014},
+    {"GBP": 0.8353},
+    {"ILS": 3.4311}], 6: [
+    {"ANG": 1.7900},
+    {"USD": 1},
+    {"AED": 3.6725},
+    {"AFN": 89.6941},
+    {"ALL": 114.2457},
+    {"AOA": 429.9365},
+    {"ARS": 129.051},
+    {"AMD": 413.2282},
+    {"AUD": 1.4426},
+    {"AWG": 1.7900},
+    {"AZN": 1.6941},
+    {"BAM": 1.9178},
+    {"BBD": 2.0000},
+    {"BTN": 79.9633},
+    {"EUR": 0.9806},
+    {"FJD": 2.2014},
+    {"GBP": 0.8353},
+    {"ILS": 3.4311}], 7: [
+    {"ANG": 1.7900},
+    {"USD": 1},
+    {"AED": 3.6725},
+    {"AFN": 89.6941},
+    {"ALL": 114.2457},
+    {"AOA": 429.9365},
+    {"ARS": 129.051},
+    {"AMD": 413.2282},
+    {"AUD": 1.4426},
+    {"AWG": 1.7900},
+    {"AZN": 1.6941},
+    {"BAM": 1.9178},
+    {"BBD": 2.0000},
+    {"BTN": 79.9633},
+    {"EUR": 0.9806},
+    {"FJD": 2.2014},
+    {"GBP": 0.8353},
+    {"ILS": 3.4311}]}
 
 currencies = [
     {"ANG": 1.7900},
@@ -20,31 +149,43 @@ currencies = [
     {"BBD": 2.0000},
     {"BTN": 79.9633},
     {"EUR": 0.9806},
-    {"FJD":2.2014},
+    {"FJD": 2.2014},
     {"GBP": 0.8353},
     {"ILS": 3.4311}]
 
-@app.route('/', methods=['GET', 'POST'])
-def getUsers():
+
+@app.route('/clear/<int:team_id>', methods=['GET'])
+def clear_list(team_id):
+    dic_currencies[team_id] =copy.deepcopy(currencies)
+    return 'Done',200
+
+
+@app.route('/currency/<int:team_id>', methods=['GET', 'POST'])
+def getUsers(team_id):
     if request.method == 'GET':
-        return json.dumps(currencies)
+        return json.dumps(dic_currencies[team_id])
     elif request.method == 'POST':
         currency = request.get_json()
-        # user["id"]=request.form['id';
-        # user["id"]=request;
-        # user["id"]=re;
-        print(currency)
-        currencies.append(currency)
-        return json.dumps(currencies)
-
-
-@app.route('/users/<int:id>', methods=['GET'])
-def getUSerById(id):
-    if request.method == 'GET':
-        if id < len(currencies):
-            return json.dumps(currencies[id - 1])
+        for c in dic_currencies[team_id]:
+            if c.keys() == currency.keys() or list(currency.values())[0] <= 0:
+                return 'currency Exist or the value is not valid 404 ', 404
         else:
-            return 'we dont have this currency '
+            dic_currencies[team_id].append(currency)
+        return json.dumps(dic_currencies[team_id]), 201
+
+
+@app.route('/update_currency/<int:team_id>', methods=['POST'])
+def update_currency(team_id):
+    currency = request.get_json()
+    if currency in dic_currencies[team_id]:
+        key = list(currency.keys())[0]
+        for c in dic_currencies[team_id]:
+            if list(c.keys())[0] == key:
+                c[key] = currency[key]
+                return 'created',201
+    else:
+        dic_currencies[team_id].append(currency)
+    return json.dumps(dic_currencies[team_id])
 
 
 app.run()
